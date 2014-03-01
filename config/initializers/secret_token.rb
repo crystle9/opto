4f93772db2f72e4950b31bb72fb211b9dae9aaf4
crystle9 <cry_b@126.com>
2014-03-01 13:51:34 +0800

    born

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogOfCrystle9::Application.config.secret_token = '307c4947ba62891adec634b1ae68a1034d351e0ce76b34d3f4695286d68d692125a7cc29416588f02566e1c1a7a8fe28717340ce99f934e659cc7cb8066e1562'
