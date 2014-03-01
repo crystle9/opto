9ed0283ec9303824c50db13baa7b68551582f7b3
crystle9 <cry_b@126.com>
2014-03-01 14:13:16 +0800

    post and redcloth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 BlogOfCrystle9::Application.routes.draw do
+  root :to => 'home#index'
+  get "home/index"
+
+  resources :posts
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
