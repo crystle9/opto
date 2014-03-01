8a8511be05e4246deac8722bee313ca122ed9637
crystle9 <cry_b@126.com>
2014-03-02 01:53:49 +0800

    cant it work?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ BlogOfCrystle9::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: '54.225.52.202' }
 
   # Enable threaded mode
   # config.threadsafe!
